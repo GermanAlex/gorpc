84a914620eb68091f9541301ca1decd097b0a192
Istomin German <needwolf@gmail.com>
2022-07-21 00:11:05 +1000

    release server and goProverbs file

---
+++ server/server.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"math/rand"
+	"net"
+	"os"
+	"strings"
+	"time"
+)
+
+const protocol, address = "tcp4", ":11111"
+
+func main() {
+	listener, err := net.Listen(protocol, address)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		connection, err := listener.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		go proverbsConn(connection)
+	}
+}
+
+func proverbsConn(connection net.Conn) {
+	defer connection.Close()
+	reader := bufio.NewReader(connection)
+	b, err := reader.ReadBytes('\n')
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	message := strings.TrimSuffix(string(b), "\n")
+	message = strings.TrimSuffix(message, "\r")
+
+	if message == "proverbs" {
+		result, err := getProverbs()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		connection.Write([]byte(result + "\n"))
+	}
+
+}
+
+func getProverbs() (string, error) {
+	var proverbsArr []string
+
+	file, err := os.Open("/goProverbs.txt")
+
+	if err != nil {
+		return "", err
+	}
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+	for scanner.Scan() {
+		proverbsArr = append(proverbsArr, scanner.Text())
+	}
+
+	file.Close()
+
+	rand.Seed(time.Now().Unix())
+	return proverbsArr[rand.Intn(len(proverbsArr))], nil
+}
